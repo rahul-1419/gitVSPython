5ab0381d7320f0220c9767b27b8afe2d967129f1
rahul-1419 <rahulchaudhari1452003@gmail.com>
2024-12-20 15:55:21 +0530

    1 star pattern.py

---
+++ Star Patterns/Simple_star.py
@@ -0,0 +1,17 @@
+"""
+*****
+*****
+*****
+*****
+*****
+"""
+
+def simple_star(n):
+    for i in range (1,n+1):
+        for j in range(1,n+1):
+            print("*",end="")
+        print()
+
+    else:
+        print("Think logic agin......")
+simple_star(5)
